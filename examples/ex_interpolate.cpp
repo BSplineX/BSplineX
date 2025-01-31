8752b3523a012c7a1356be0dcbaff5b923e94542
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-01-31 17:38:48 +0100

    Updated examples

--- examples/ex_interpolate.cpp
+++ examples/ex_interpolate.cpp
@@ -6,19 +6,18 @@
 int main()
 {
   // points to interpolate
-  const std::vector<double> x_interp{2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 40.2};
-  std::vector<double> y_interp{11.2, 22.3, 13.4, 14.5, 25.6, 36.7, 17.8, 11.2};
+  std::vector<double> const x_interp{2.3, 3.4, 4.5, 5.6, 6.7, 7.8, 8.9, 40.2};
+  std::vector<double> const y_interp{11.2, 22.3, 13.4, 14.5, 25.6, 36.7, 17.8, 11.2};
 
-  constexpr std::size_t degree{3};
-  const std::vector<double> knots{0.1, 1.3, 2.2, 2.2, 4.9, 6.3, 6.3, 6.3, 13.2};
+  constexpr size_t degree{2};
 
-  auto bspline = bsplinex::factory::periodic_nonuniform(degree, knots);
+  auto bspline = bsplinex::factory::open_nonuniform(degree);
 
   // interpolate the curve to the points
-  bspline.interpolate(x_interp, y_interp, {});
+  bspline.interpolate(x_interp, y_interp, {{6.7, 25.6, 0}});
 
   double x, y;
-  for (size_t i{0}; i < x_interp.size(); i++)
+  for (size_t i{degree}; i < x_interp.size() - degree; i++)
   {
     x = x_interp.at(i);
     y = y_interp.at(i);
