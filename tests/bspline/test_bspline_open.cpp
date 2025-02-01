8fc03ee6e19f8fd1bcf31444337db4d0fef7ff6c
Sebastiano Taddei <61469576+SebastianoTaddei@users.noreply.github.com>
2025-02-01 18:13:48 +0100

    Improved testing for open bspline

--- tests/bspline/test_bspline_open.cpp
+++ tests/bspline/test_bspline_open.cpp
@@ -1,9 +1,11 @@
 // Third-party includes
+#include <algorithm>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 #include <random>
 
 // BSplineX includes
+#include "BSplineX/bspline/bspline_factory_open.hpp"
 #include "BSplineX/bspline/bspline_lsq.hpp"
 #include "BSplineX/bspline/bspline_types.hpp"
 
@@ -11,8 +13,31 @@ using namespace Catch::Matchers;
 using namespace bsplinex;
 using namespace bsplinex::bspline;
 
+static std::mt19937 &get_device()
+{
+  static std::random_device device{};
+  static std::mt19937 rng{device()};
+
+  rng.seed(05535);
+
+  return rng;
+}
+
+static std::vector<double>
+random_vector(std::mt19937 &rng, size_t num_elems, double min = -10.0, double max = 10.0)
+{
+  std::uniform_real_distribution unif{min, max};
+  std::vector<double> vec(num_elems);
+  std::generate(vec.begin(), vec.end(), [&unif, &rng]() { return unif(rng); });
+
+  return vec;
+}
+
 TEST_CASE("OpenNonUniform", "[bspline]")
 {
+  auto &rng = get_device();
+
+  // Normal BSpline
   size_t degree{3};
 
   std::vector<double> t_data_vec{0.1, 1.3, 2.2, 2.2, 4.9, 6.3, 6.3, 6.3, 13.2};
@@ -27,6 +52,29 @@ TEST_CASE("OpenNonUniform", "[bspline]")
   std::vector<double> x_values{2.2, 2.3000000000000003, 2.4000000000000004, 2.5000000000000004, 2.6000000000000005, 2.7000000000000006, 2.8000000000000007, 2.900000000000001, 3.000000000000001, 3.100000000000001, 3.200000000000001, 3.300000000000001, 3.4000000000000012, 3.5000000000000013, 3.6000000000000014, 3.7000000000000015, 3.8000000000000016, 3.9000000000000017, 4.000000000000002, 4.100000000000001, 4.200000000000002, 4.3000000000000025, 4.400000000000002, 4.500000000000002, 4.600000000000002, 4.700000000000003, 4.8000000000000025, 4.900000000000002, 5.000000000000003, 5.100000000000003, 5.200000000000003, 5.3000000000000025, 5.400000000000003, 5.5000000000000036, 5.600000000000003, 5.700000000000003, 5.800000000000003, 5.900000000000004, 6.0000000000000036, 6.100000000000003, 6.200000000000004};
   std::vector<double> y_values{0.4, 0.4987905929445725, 0.5953834608104188, 0.6901102371457323, 0.7833025554987059, 0.8752920494175335, 0.9664103524504085, 1.0569890981455239, 1.1473599200510731, 1.2378544517152497, 1.3288043266862466, 1.420541178512258, 1.5133966407414765, 1.6077023469220952, 1.7037899306023085, 1.8019910253303089, 1.9026372646542904, 2.0060602821224456, 2.112591711282968, 2.2225631856840518, 2.3363063388738903, 2.4541528044006755, 2.5764342158126015, 2.7034822066578617, 2.83562841048465, 2.9732044608411603, 3.1165419912755823, 3.2659726353361123, 3.4243850625148546, 3.604896086079547, 3.8231795552418366, 4.094909319213373, 4.435759227205808, 4.861403128430789, 5.387514872099959, 6.029768307424969, 6.8038372836174785, 7.725395649889126, 8.810117255451555, 10.073675949516419, 11.53174558129538};
   // clang-format on
+
+  // Big BSpline
+  size_t const num_ctrl{600};
+  size_t const num_knots{num_ctrl + degree + 1};
+  std::vector<double> big_ctrl_pts = random_vector(rng, num_ctrl, -1.0, 1.0);
+  std::vector<double> big_knots    = random_vector(rng, num_knots, -10.0, 10.0);
+
+  std::sort(big_knots.begin(), big_knots.end());
+
+  types::OpenNonUniform<double> big_bspline =
+      factory::open_nonuniform(degree, big_knots, big_ctrl_pts);
+
+  size_t const num_elems{2000};
+  std::vector<double> big_x = random_vector(
+      rng, num_elems, big_knots.at(degree), big_knots.at(big_knots.size() - 1 - degree)
+  );
+  std::sort(big_x.begin(), big_x.end());
+  std::vector<double> big_y(num_elems);
+  std::generate(
+      big_y.begin(),
+      big_y.end(),
+      [i = 0, &big_bspline, &big_x]() mutable { return big_bspline.evaluate(big_x.at(i++)); }
+  );
 
   SECTION("evaluate(...)")
   {
@@ -54,11 +102,7 @@ TEST_CASE("OpenNonUniform", "[bspline]")
   SECTION("fit(...) - dense")
   {
     bspline.fit(x_values, y_values);
-    auto control_points = bspline.get_control_points();
-    for (size_t i{0}; i < c_data.size(); i++)
-    {
-      REQUIRE_THAT(control_points.at(i), WithinRel(c_data.at(i)));
-    }
+
     for (size_t i{0}; i < x_values.size(); i++)
     {
       REQUIRE_THAT(bspline.evaluate(x_values.at(i)), WithinRel(y_values.at(i)));
@@ -67,41 +111,9 @@ TEST_CASE("OpenNonUniform", "[bspline]")
 
   SECTION("fit(...) - sparse")
   {
-    // Prepare a normal distribution
-    std::random_device device{};
-    std::mt19937 rng{device()};
-    rng.seed(05535);
-    std::normal_distribution norm{0.0, 1.0};
-
-    // Generated big knots and ctrl points
-    std::vector<double> big_ctrl_pts(513);
-    std::vector<double> big_knots(big_ctrl_pts.size() + degree + 1);
-    std::generate(big_ctrl_pts.begin(), big_ctrl_pts.end(), [&norm, &rng]() { return norm(rng); });
-    std::generate(big_knots.begin(), big_knots.end(), [n = 0]() mutable { return (double)n++; });
-
-    types::OpenNonUniform<double> big_bspline{big_knots, big_ctrl_pts, degree};
-
-    // Prepare a uniform distribution
-    std::uniform_real_distribution unif{
-        big_knots.at(degree), big_knots.at(big_knots.size() - 1 - degree)
-    };
-
-    // Randomly sample points
-    std::vector<double> big_x(2000);
-    std::vector<double> big_y(big_x.size());
-    std::generate(big_x.begin(), big_x.end(), [&unif, &rng]() { return unif(rng); });
-    std::generate(
-        big_y.begin(),
-        big_y.end(),
-        [i = 0, &big_bspline, &big_x]() mutable { return big_bspline.evaluate(big_x.at(i++)); }
-    );
 
     big_bspline.fit(big_x, big_y);
-    auto control_points = big_bspline.get_control_points();
-    for (size_t i{0}; i < big_ctrl_pts.size(); i++)
-    {
-      REQUIRE_THAT(control_points.at(i), WithinRel(big_ctrl_pts.at(i), 1e-12));
-    }
+
     for (size_t i{0}; i < big_x.size(); i++)
     {
       REQUIRE_THAT(big_bspline.evaluate(big_x.at(i)), WithinRel(big_y.at(i), 1e-12));
@@ -115,10 +127,27 @@ TEST_CASE("OpenNonUniform", "[bspline]")
     {
       additional.emplace_back(x_values.at(degree + i), y_values.at(degree + i), 0);
     }
+
     bspline.interpolate(x_values, y_values, additional);
     for (size_t i{degree}; i < x_values.size() - degree; i++)
     {
       REQUIRE_THAT(bspline.evaluate(x_values.at(i)), WithinRel(y_values.at(i)));
+    }
+  }
+
+  SECTION("interpolate(...) - sparse")
+  {
+    std::vector<lsq::Condition<double>> additional;
+    for (size_t i{0}; i < degree - 1; i++)
+    {
+      additional.emplace_back(big_x.at(degree + i), big_y.at(degree + i), 0);
+    }
+
+    big_bspline.interpolate(big_x, big_y, additional);
+
+    for (size_t i{degree}; i < big_x.size() - degree; i++)
+    {
+      REQUIRE_THAT(big_bspline.evaluate(big_x.at(i)), WithinRel(big_y.at(i), 1e-10));
     }
   }
 
